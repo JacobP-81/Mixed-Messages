1dc0f616a32b56b42c83a8deced3c932e648a403
Jacob P <70002370+JacobP-81@users.noreply.github.com>
2021-06-04 16:07:04 -0400

    Update Mixed Messages.js

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,6 +1,6 @@
 /* eslint-disable strict */
 const mixedMessages = {
-    joke1: [],
+    joke1: ['she hides the sun','when she got on the scale it said, "I need your weight not your phone number."',' that when she went to the beach a whale swam up and sang, "We are family, even though you're fatter than me."','when God said, "Let there be light," he asked your mother to move out of the way.' ],
     joke2: [],
     joke3: [],
 }
@@ -15,7 +15,7 @@ const makeJokes = () => {
     for (let arr in mixedMessages) {
         let jokes = random(mixedMessages[arr].length)
         switch (arr) {
-            case 'joke1': phrases.push(`   ${mixedMessages[arr][jokes]}`)
+            case 'joke1': phrases.push(`Yo mama is so fat, ${mixedMessages[arr][jokes]}`)
                 break;
             case 'joke2': phrases.push(`   ${mixedMessages[arr][jokes]}`)
                 break;
